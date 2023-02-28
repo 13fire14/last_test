66663631b35a68ece28a2bf1ef196167d4a7d44d
13fire14 <105860623+13fire14@users.noreply.github.com>
2023-02-28 15:12:33 +0800

    Update last_test.py

--- last_test.py
+++ last_test.py
@@ -287,7 +287,7 @@ def get_biqu_all_book(user_agent,n1,n2):
     #             url=(fenlei_url[i]+f'{j+1}'+'.html')
     #             break
     all_page=get_biqu_fenlei_page(url,user_agent)
-    #st.write(f'总的有{all_page}页')
+    st.write(f'总的有{all_page}页')
     for j in (range(n1,n2)):
         if j%20==0:
             st.write(f'{j}/{all_page}')
@@ -349,8 +349,8 @@ def tool_box():
     elif choose=='一键插入标题行'  :
         user_data_load(column)
     elif choose=='更新笔趣书目前300页':
-        n1=int(st.text_input('请输入从第几页开始:'))
-        n2=int(st.text_input('请输入从第几页结束:'))
+        n1=(st.text_input('请输入从第几页开始:'))
+        n2=(st.text_input('请输入从第几页结束:'))
         if n1=="" and n2=="":
             st.stop()
         st.success(get_biqu_all_book(user_agent,n1,n2))
