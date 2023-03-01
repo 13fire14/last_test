d2526c6e9d1cf40904635eee59ede38a55aa1411
13fire14 <105860623+13fire14@users.noreply.github.com>
2023-03-01 11:15:06 +0800

    Update last_test.py

--- last_test.py
+++ last_test.py
@@ -403,8 +403,8 @@ def tool_box():
     elif choose=='查看51书城书目':
         show_51_book()
     elif choose=="查看笔趣书目":
-        data=get_all_book()
-        st.download_button('保存目录',data,file_name='book.csv')
+        data_look=get_all_book()
+        st.download_button('保存目录',data_look,file_name='book.csv')
     elif choose=='删除笔趣书目':
         code11=st.text_input('请输入删除的密码：')
         if code11!='zwz':
