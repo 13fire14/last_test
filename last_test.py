6d3f002305af4e4769af359df179fe7928d30a4a
13fire14 <105860623+13fire14@users.noreply.github.com>
2023-03-02 22:07:23 +0800

    Update last_test.py

--- last_test.py
+++ last_test.py
@@ -356,7 +356,7 @@ def get_all_book(name):
                 data1=pd.read_csv(txt)
                 #st.dataframe(data1)
                 biqu_data=pd.concat([biqu_data,data1])
-   # st.dataframe(biqu_data)
+    st.dataframe(biqu_data)
     #st.write(biqu_data.head(n))
     return biqu_data
 #%% 删除笔趣书目
